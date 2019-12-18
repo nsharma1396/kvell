39fc35e4080c38fb5d5b1aee6d96fa4621cec7fc
Neeraj <n.sharma1396@gmail.com>
2019-12-18 20:34:31 +0530

    Create basic configurations

---
+++ packages/eslint-config-tek-node/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended"],
+  plugins: ["node"],
+  env: {
+    node: true,
+    mongo: true,
+    mocha: true,
+    es6: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020
+    // ecmaFeatures: {}
+  },
+  rules: {
+    "no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_|^req|^res|^next|^err", ignoreRestSiblings: true }
+    ]
+  }
+};
