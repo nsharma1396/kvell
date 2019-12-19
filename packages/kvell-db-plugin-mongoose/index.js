25314b884e8dadc77dcb916030b03cf94a7fe4a7
Neeraj <n.sharma1396@gmail.com>
2019-12-19 20:18:09 +0530

    feat: add jsdoc types for better type definitions ( helps in vscode intellisense too )

--- packages/kvell-db-plugin-mongoose/index.js
+++ packages/kvell-db-plugin-mongoose/index.js
@@ -1,5 +1,15 @@
+/**
+ * @type {import ("mongoose")}
+ */
 const mongoose = require("mongoose");
+/**
+ * @type {import ("mongoose")}
+ */
 const dbInstance = require("./init");
+
+/**
+ * @type {() => Promise}
+ */
 const initHandler = require("./syncHandler");
 
 module.exports = {
