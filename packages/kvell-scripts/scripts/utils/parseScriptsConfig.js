c637bc555d67a5f9b55e2fd66630d5c193f4711c
Neeraj <n.sharma1396@gmail.com>
2019-12-19 20:48:42 +0530

    Update parseScriptsConfig to use `kvell.config.js`

--- packages/kvell-scripts/scripts/utils/parseScriptsConfig.js
+++ packages/kvell-scripts/scripts/utils/parseScriptsConfig.js
@@ -2,9 +2,13 @@ const path = require("path");
 const readFileFromPath = require("./fsWrapper").readFromFile;
 const parsedConfigInit = require("../config/appDefaultConfig");
 
-const configFileName = "nodescript.config.json";
+const configFileName = "kvell.config.js";
 
-const configErrorKeys = ["routes", "protocol", "models"];
+const configErrorKeys = ["routes", "protocol", "models", "database"];
+
+const isObject = obj => {
+  return !Array.isArray(obj) && obj === Object(obj);
+};
 
 const createConfigObject = parsedConfigs => {
   const configKeysStatus = {
@@ -18,13 +22,16 @@ const createConfigObject = parsedConfigs => {
     },
     models: {
       valid: !parsedConfigs.models ? true : Array.isArray(parsedConfigs.models)
+    },
+    database: {
+      valid: !parsedConfigs.database ? true : isObject(parsedConfigs.database)
     }
   };
 
-  const { routes, protocol, models } = configKeysStatus;
+  const { routes, protocol, models, database } = configKeysStatus;
 
   let configObject = {};
-  if (!routes.valid || !protocol.valid || !models) {
+  if (!routes.valid || !protocol.valid || !models.valid || !database.valid) {
     const errorString = `Your ./${configFileName} contains invalid config values for:\n${configErrorKeys
       .filter(configKey => !configKeysStatus[configKey].valid)
       .join("\n")}`;
@@ -33,7 +40,8 @@ const createConfigObject = parsedConfigs => {
     configObject = {
       routes: parsedConfigs.routes || parsedConfigInit.routes,
       protocol: parsedConfigs.protocol || parsedConfigInit.protocol,
-      models: parsedConfigs.models || parsedConfigInit.models
+      models: parsedConfigs.models || parsedConfigInit.models,
+      database: parsedConfigs.database || parsedConfigInit.database
     };
   }
   return configObject;
@@ -41,17 +49,10 @@ const createConfigObject = parsedConfigs => {
 
 const parseScriptsConfig = async appSrcDirectory => {
   let parsedConfig = parsedConfigInit;
-  const configFilePath = path.relative(appSrcDirectory, `./${configFileName}`);
-  // try {
-  const configObject = await readFileFromPath(configFilePath, "UTF-8");
-  const parsedConfigObject = JSON.parse(configObject);
+  const configFilePath = path.join(appSrcDirectory, `./${configFileName}`);
+  const parsedConfigObject = require(configFilePath);
   parsedConfig = createConfigObject(parsedConfigObject);
   return parsedConfig;
-  // } catch (e) {
-  //   console.log(e);
-
-  //   return parsedConfigInit;
-  // }
 };
 
 module.exports = parseScriptsConfig;
