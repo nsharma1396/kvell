eec40b40dd0e1fcf5092303925c60e2549ea20ea
Neeraj <n.sharma1396@gmail.com>
2019-12-19 15:49:13 +0530

    Feat: Add jsdocs type definitions ( This helps in better intellisense for vscode too )

--- packages/kvell-db-plugin-sequelize/index.js
+++ packages/kvell-db-plugin-sequelize/index.js
@@ -1,5 +1,15 @@
+/**
+ * @type {import ("sequelize")}
+ */
 const Sequelize = require("sequelize");
+/**
+ * @type {import ("sequelize").Sequelize}
+ */
 const dbInstance = require("./init");
+
+/**
+ * @type {() => Promise}
+ */
 const initHandler = require("./syncHandler");
 
 module.exports = {
