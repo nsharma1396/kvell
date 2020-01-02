3ca627c95bc5a4db32f44ff624f0f31c2d1a2867
Neeraj <n.sharma1396@gmail.com>
2020-01-02 13:21:45 +0530

    Update `globalMiddlewares` with `server` param

--- packages/create-kvell-app/templates/basic/global/globalMiddlewares.js
+++ packages/create-kvell-app/templates/basic/global/globalMiddlewares.js
@@ -1,7 +1,9 @@
 /**
- *
+ * `globalMiddlewares` handles all the middlewares/functions/configurations that you need
+ * to declare/use in your application globally.
  * @param {import ("kvell-scripts").KvellAppObject} app
+ * @param {import ("kvell-scripts").KvellServerObject} server
  */
-const globalMiddlewares = app => {};
+const globalMiddlewares = (app, server) => {};
 
 module.exports = globalMiddlewares;
