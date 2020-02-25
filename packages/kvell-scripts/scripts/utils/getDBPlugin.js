8ee6f8769d3f9bd3a30e8d70aab924cf33b8c391
Neeraj <n.sharma1396@gmail.com>
2020-02-25 19:37:14 +0530

    Reimplement getDBPlugin based on promises

--- packages/kvell-scripts/scripts/utils/getDBPlugin.js
+++ packages/kvell-scripts/scripts/utils/getDBPlugin.js
@@ -3,19 +3,26 @@ const path = require("path");
 /**
  * @param {{resolve: string, options: object}[]} pluginsArray
  */
-const getDBPlugins = pluginsArray => {
+async function resolvePlugins(plugins) {
   const syncHandlers = {};
-  pluginsArray.forEach(plugin => {
+  for (let plugin of plugins) {
     // Import the instance from "plugin/init" first so that instance is created before the main is required
     const pluginInstancePath = path.resolve(process.cwd(), "node_modules", plugin.resolve, "init");
     const PluginInstance = require(pluginInstancePath);
-    PluginInstance.createDBInstance(plugin.options);
+    await PluginInstance.createDBInstance(plugin.options);
 
     const pluginPath = path.resolve(process.cwd(), "node_modules", plugin.resolve);
     const pluginExport = require(pluginPath);
     syncHandlers[plugin.resolve] = pluginExport.initHandler;
-  });
+  }
   return syncHandlers;
+}
+
+/**
+ * @param {{resolve: string, options: object}[]} pluginsArray
+ */
+const getDBPlugins = pluginsArray => {
+  return resolvePlugins(pluginsArray);
 };
 
 module.exports = getDBPlugins;
