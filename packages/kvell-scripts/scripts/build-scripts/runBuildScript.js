99227a8e33050520580e08db368d8202af5218af
Neeraj <n.sharma1396@gmail.com>
2020-04-09 01:06:46 +0530

    Update api to run without --name option

--- packages/kvell-scripts/scripts/build-scripts/runBuildScript.js
+++ packages/kvell-scripts/scripts/build-scripts/runBuildScript.js
@@ -1,12 +1,13 @@
 const path = require("path");
 const chalk = require("chalk");
 const pm2 = require("pm2");
-const arg = require("arg");
+// const arg = require("arg");
+const fs = require("fs");
 const log = console.log;
 
 let buildScripts = ["start", "stop", "delete", "status", "restart", "flush", "startup", "list"];
 
-// const processName = "kvell-scripts-app";
+// const processPm_id = "kvell-scripts-app";
 
 const runBuildScript = (script, nodeArgs) => {
   let buildScript;
@@ -24,26 +25,53 @@ const runBuildScript = (script, nodeArgs) => {
 
     const scriptPath = require.resolve(path.join("..", "..", "scripts", script));
 
-    // // Append the file path only if it is needed by the pm2 api
+    let processPID,
+      processPm_id,
+      delimeter = "%____%",
+      fileExists = false;
+
+    try {
+      const pathToPid = path.join(process.cwd(), "logs", "build-logs", "build_pm.pid");
+      if (fs.existsSync(pathToPid)) {
+        [processPID, processPm_id] = fs.readFileSync(pathToPid, "utf-8").split(delimeter);
+        if (
+          fs.existsSync(path.join(process.cwd(), "logs", "build-logs", `build-${processPm_id}.pid`))
+        ) {
+          fileExists = true;
+        }
+      }
+    } catch (err) {
+      fileExists = false;
+    }
+
+    // Append the file path only if it is needed by the pm2 api
     // if (buildScript === "start" || buildScript === "stop" || buildScript === "restart") {
     //   scriptsArray = scriptsArray.concat(scriptPath);
     // }
 
     // const nodeCommand = "pm2";
-    const args = arg(
-      {
-        "--name": String
-      },
-      {
-        argv: nodeArgs,
-        permissive: false
-      }
-    );
+    // const args = arg(
+    //   {
+    //     "--name": String
+    //   },
+    //   {
+    //     argv: nodeArgs,
+    //     permissive: false
+    //   }
+    // );
 
-    const processName = args["--name"];
+    // const processName = args["--name"];
 
-    if (processName || buildScript === "startup" || buildScript === "list") {
-      pm2.connect(function(err) {
+    // To-do: simplify the condition :|
+    if (
+      (!fileExists &&
+        (processPm_id ||
+          buildScript === "start" ||
+          buildScript === "startup" ||
+          buildScript === "list")) ||
+      (fileExists && buildScript !== "start")
+    ) {
+      pm2.connect(function (err) {
         if (err) {
           console.error(err);
           process.exit(2);
@@ -54,26 +82,32 @@ const runBuildScript = (script, nodeArgs) => {
           const logDirPaths = {
             output: path.join(process.cwd(), "logs", "build-logs", "build_output.log"),
             error: path.join(process.cwd(), "logs", "build-logs", "build_errors.log"),
-            pid: path.join(process.cwd(), "logs", "build-logs", "build-name-id.pid")
+            pid: path.join(process.cwd(), "logs", "build-logs", "build.pid"),
+            pm_id: path.join(process.cwd(), "logs", "build-logs", "build_pm.pid"),
           };
 
           pm2.start(
             {
-              name: processName,
+              // name: processPm_id,
               script: scriptPath,
               cwd: process.cwd(),
               output: logDirPaths.output,
               error: logDirPaths.error,
-              pid: logDirPaths.pid
+              pid: logDirPaths.pid,
               // exec_mode: "cluster", // Allows your app to be clustered
               // instances: 4, // Optional: Scales your app by 4
               // max_memory_restart: "100M" // Optional: Restarts your app if it reaches 100Mo
             },
-            function(err, proc) {
+            function (err, proc) {
               pm2.disconnect(); // Disconnects from PM2
               if (err) {
                 throw err;
               }
+
+              fs.writeFileSync(
+                `${logDirPaths.pm_id}`,
+                `${proc[0].pid}${delimeter}${proc[0].pm2_env.pm_id}`
+              );
 
               log("Process details:\n");
 
@@ -92,11 +126,13 @@ const runBuildScript = (script, nodeArgs) => {
         } else if (buildScript === "status") {
           log(chalk.green("Loading status for production server...\n"));
 
-          pm2.describe(processName, function(err, proc) {
+          pm2.describe(processPm_id, function (err, proc) {
             pm2.disconnect(); // Disconnects from PM2
             if (err) {
               throw err;
             }
+
+            console.log(proc);
 
             if (proc && proc[0]) {
               const processStatus = proc[0].pm2_env.status;
@@ -112,14 +148,14 @@ const runBuildScript = (script, nodeArgs) => {
             } else {
               log(
                 `${chalk.bgRedBright.whiteBright("ERROR!")} ${chalk.redBright(
-                  `No build processes found with the name ${processName}\n`
+                  `No build processes found with the id ${processPm_id}\n`
                 )}`
               );
             }
           });
         } else if (buildScript === "stop") {
           log(chalk.green("Stopping the production server...\n"));
-          pm2.stop(processName, (err, proc) => {
+          pm2.stop(processPm_id, (err, proc) => {
             pm2.disconnect(); // Disconnects from PM2
             if (err) {
               throw err;
@@ -131,11 +167,14 @@ const runBuildScript = (script, nodeArgs) => {
         } else if (buildScript === "delete") {
           log(chalk.green("Deleting pm2 process for production server...\n"));
 
-          pm2.delete(processName, function(err, proc) {
+          pm2.delete(processPm_id, function (err, proc) {
             pm2.disconnect(); // Disconnects from PM2
             if (err) {
               throw err;
             }
+
+            const pm_idFile = path.join(process.cwd(), "logs", "build-logs", "build_pm.pid");
+            fs.unlinkSync(pm_idFile);
 
             log(proc);
             log();
@@ -143,7 +182,7 @@ const runBuildScript = (script, nodeArgs) => {
           });
         } else if (buildScript === "restart") {
           log(chalk.green("Restarting the production server...\n"));
-          pm2.restart(processName, (err, proc) => {
+          pm2.restart(processPm_id, (err, proc) => {
             pm2.disconnect(); // Disconnects from PM2
             if (err) {
               throw err;
@@ -154,7 +193,7 @@ const runBuildScript = (script, nodeArgs) => {
           });
           // } else if (buildScript === "reload") {
           //   log(chalk.green("Restarting the production server...\n"));
-          //   pm2.reload(processName, (err, proc) => {
+          //   pm2.reload(processPm_id, (err, proc) => {
           //     pm2.disconnect(); // Disconnects from PM2
           //     if (err) {
           //       throw err;
@@ -165,7 +204,7 @@ const runBuildScript = (script, nodeArgs) => {
           //   });
         } else if (buildScript === "startup") {
           // log(chalk.green("Restarting the production server...\n"));
-          pm2.startup(nodeArgs[0], err => {
+          pm2.startup(nodeArgs[0], (err) => {
             pm2.disconnect(); // Disconnects from PM2
             if (err) {
               throw err;
@@ -173,7 +212,7 @@ const runBuildScript = (script, nodeArgs) => {
           });
         } else if (buildScript === "flush") {
           log(chalk.yellow("Flushing all the logs...\n"));
-          pm2.flush(processName, function(err, result) {
+          pm2.flush(processPm_id, function (err, result) {
             pm2.disconnect(); // Disconnects from PM2
             if (err) {
               throw err;
@@ -196,11 +235,27 @@ const runBuildScript = (script, nodeArgs) => {
         }
       });
     } else {
-      log(
-        `${chalk.bgRedBright.whiteBright("ERROR!")}  ${chalk.redBright(
-          "Please provide a process name with the --name option"
-        )}\n`
-      );
+      if (fileExists && buildScript === "start") {
+        log(
+          `${chalk.bgRedBright.whiteBright("ERROR!")}  ${chalk.redBright(
+            "Production server already running with pid: ",
+            processPID
+          )}\n`
+        );
+      } else if (!fileExists && (buildScript !== "startup" || buildScript !== "list")) {
+        log(
+          `${chalk.bgRedBright.whiteBright("ERROR!")}  ${chalk.redBright(
+            "No build process exists for this application."
+          )}\n`
+        );
+      } else {
+        log(
+          `${chalk.bgRedBright.whiteBright("ERROR!")}  ${chalk.redBright(
+            "There was an error while executing " + buildScript + " script."
+            // "Please provide a process name with the --name option"
+          )}\n`
+        );
+      }
     }
   } else {
     log(
